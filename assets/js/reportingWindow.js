519f0200fb5363c29bd05520dfecc02973b3722b
Signore Oladayo <dannapel@gmail.com>
2018-08-16 22:59:38 +0100

    Finished the reporting module

--- assets/js/reportingWindow.js
+++ assets/js/reportingWindow.js
@@ -32,7 +32,7 @@ $('#rangeForm').on('submit', function(event){
         })
     }
 
-    if ($('#not-completed').prop('checked') == false) {
+    if ($('#uncompleted').prop('checked') == true) {
         Task.findAll({
             where:{
                 status: 0,
@@ -42,7 +42,99 @@ $('#rangeForm').on('submit', function(event){
             }
         })
         .then(tasks => {
-            console.log(tasks)
+            //open a new window
+            let payload = {
+                type: 'not-completed',
+                tasks: tasks,
+                start: start,
+                finish: finish
+            }
+            ipcRenderer.send('got-results', payload)
         })
     }
+
+
+    if ($('#overdue').prop('checked') == true) {
+        Task.findAll({
+            where:{
+                status: 2,
+                createdAt: {
+                    $between: [start, finish]
+                }
+            }
+        })
+        .then(tasks => {
+            //open a new window
+            let payload = {
+                type: 'overdue',
+                tasks: tasks,
+                start: start,
+                finish: finish
+            }
+            ipcRenderer.send('got-results', payload)
+        })
+    }
+
+})
+
+//for the title search form
+$('#title-search').on('submit', function(event){
+    event.preventDefault();
+    let searchParams = $('#search-bar').val();
+
+    Task.findAll({
+        where: {
+            title: {
+                $like: '%'+searchParams+'%'
+            }
+        }
+    })
+    .then(tasks => {
+        let payload = {
+            type: 'title-search',
+            tasks: tasks,
+            searchParams: searchParams
+        }
+
+        ipcRenderer.send('got-results', payload)
+    })
+})
+
+$('#generate').on('click', function(event){
+    event.preventDefault();
+
+    Task.findAll()
+        .then(tasks => {
+            let completed = 0;
+            let uncompleted = 0;
+            let overdue = 0;
+            tasks.map(task => {
+                
+                if (task.dataValues.status == 0) {
+                    //uncompleted
+                    uncompleted = uncompleted + 1
+                } 
+
+                if (task.dataValues.status == 1) {
+                    //uncompleted
+                    completed = completed + 1
+                } 
+
+                if (task.dataValues.status == 2) {
+                    //uncompleted
+                    overdue = overdue + 1
+                } 
+            })
+
+            let payload= {
+                type: 'chart',
+                completed: completed,
+                uncompleted: uncompleted,
+                overdue: overdue
+            }
+
+            console.log(payload)
+
+            ipcRenderer.send('got-results', payload)
+    })
 })
--- assets/js/results.js
+++ assets/js/results.js
@@ -1,28 +1,85 @@
 
 const ipcRenderer = require('electron').ipcRenderer
+const Chart = require('chart.js')
 
 ipcRenderer.on('results', function(event,payload){
     
     if (payload.type == 'completed') {
-        $('#report-title').text('Report showing all completed tasks from '+new Date(payload.start).toDateString()+' to '+new Date(payload.finish).toDateString())
+        $('#graphical').css('display', 'none')
+        $('#report-title').text('Report showing all Completed Tasks from '+new Date(payload.start).toDateString()+' to '+new Date(payload.finish).toDateString())
+
+        appendDataToTable(payload)
     }
 
     if (payload.type == 'not-completed') {
-        $('#report-title').text('Report showing all uncompleted tasks from '+new Date(payload.start).toDateString()+' to '+new Date(payload.finish).toDateString())
+        $('#graphical').css('display', 'none')
+        $('#report-title').text('Report showing all uncompleted Tasks from '+new Date(payload.start).toDateString()+' to '+new Date(payload.finish).toDateString())
+
+        appendDataToTable(payload)
+    }
+
+    if (payload.type == 'overdue') {
+        $('#graphical').css('display', 'none')
+        $('#report-title').text('Report showing all Overdue Tasks from '+new Date(payload.start).toDateString()+' to '+new Date(payload.finish).toDateString())
+
+        appendDataToTable(payload)
+    }
+
+    if (payload.type == 'title-search') {
+        $('#graphical').css('display', 'none')
+        $('#report-title').text('Search results for "'+payload.searchParams+'"')
+
+        appendDataToTable(payload)
+    }
+
+    if (payload.type == 'chart') {
+        $('#report-title').text('A graphical analysis of all the Tasks so far.')
+
+        //hide the table element
+        $('#results-table').css('display', 'none')
+
+        let chart = $('#resultsChart')
+
+        let displayChart = new Chart(chart, {
+            type: 'pie',
+            data: {
+                labels: ['Completed', 'Uncompleted', 'Overdue'],
+                datasets: [{
+                    data:[payload.completed, payload.uncompleted, payload.overdue],
+                    backgroundColor: [
+                        'green',
+                        'orange',
+                        'red'
+                    ]
+                }]
+            }
+        })
+
+        $('#completedTasks').text(payload.completed+' Tasks')
+        $('#uncompletedTasks').text(payload.uncompleted+' Tasks')
+        $('#overdueTasks').text(payload.overdue+' Tasks')
     }
     
-    let tasks = [];
-
-    payload.tasks.map(function(task){
-        tasks.push(task.dataValues)
-    })
     
-    $('#results-table').DataTable( {
-        data: tasks,
-        columns: [
-            { data: 'title' },
-            { data: 'description' },
-            { data: 'dueDate' }
-        ]
-    } );
+    function appendDataToTable(payload) {
+        let tasks = [];
+
+        payload.tasks.map(function(task){
+            tasks.push(task.dataValues)
+        })
+        
+        $('#results-table').DataTable( {
+            data: tasks,
+            columns: [
+                { data: 'title' },
+                { data: 'description' },
+                { data: 'dueDate' },
+                { data: 'reminderDate' }
+            ],
+            dom: 'Bfrtip',
+            buttons: [
+                'pdfHtml5'
+            ]
+        });
+    }
 })
